9f2a5dabb47252839fb8f3891766807013ef4729
DavidO28 <damers22@yahoo.com>
2025-01-06 12:35:40 +0400

    adjusted slider

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,37 +1,42 @@
 <template>
-  <v-divider><h2 class="mb-3">Upcoming movies</h2></v-divider>
-  <v-window
-    v-model="window"
-    show-arrows
+  <h2 class="mb-3 text-center">Upcoming movies</h2>
+  <v-carousel
+    class="mt-3 pt-3"
+    height="400"
+    progress="primary"
+    hide-delimiters
+    v-model="currentSlide"
   >
-    <v-window-item
-      v-for="item in listData"
-      :key="item"
+    <v-carousel-item
+      v-for="(item, i) in listData"
+      :key="i"
     >
-      <v-card
-        class="d-flex justify-center align-center"
-        height="400"
-      >
-        <img
-          :src="`https://image.tmdb.org/t/p/w500${item.poster_path}`"
-          class="movie-image w-100 h-100"
-          :alt="item.title"
-        />
-      </v-card>
-    </v-window-item>
-  </v-window>
+      <v-sheet height="100%">
+        <div class="d-flex fill-height justify-center align-center">
+          <span>
+            {{ item.title }}
+          </span>
+          <img
+            :src="`https://image.tmdb.org/t/p/w500${item.poster_path}`"
+            class="movie-image w-100 h-100"
+            :alt="item.title"
+          />
+          <span>{{ item.release_date }}</span>
+          <span>{{ item.total_average }}</span>
+          <span>{{ item.overview }}</span>
+        </div>
+      </v-sheet>
+    </v-carousel-item>
+  </v-carousel>
 </template>
 
 <script setup lang="ts">
   import { ref, onMounted } from 'vue'
 
-  const window = ref(0)
-  const length = 5
-
   const url = 'https://api.themoviedb.org/3/movie/upcoming'
 
   const listData = ref([])
-
+  const currentSlide = ref(0)
   const getMovies = async () => {
     try {
       const response = await fetch(url, {
@@ -50,6 +55,7 @@
 
   onMounted(() => {
     getMovies()
+    
     console.log(listData, 'slider')
   })
 </script>

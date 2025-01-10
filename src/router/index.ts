f1292106d5ea6794acd7ef1c8da3f1403a6eeae8
DavidO28 <damers22@yahoo.com>
2025-01-10 11:43:34 +0400

    added meta tags

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,36 +8,54 @@ const router = createRouter({
       name: 'home',
       component: () =>
         import(/* webpackChunkName: "home page" */ '@/views/Home.vue'),
+      meta: {
+        title: 'Movies Home',
+      },
     },
     {
       path: '/genre',
       name: 'genre',
       component: () =>
         import(/* webpackChunkName: "genre page" */ '@/views/Genre.vue'),
+      meta: {
+        title: 'Genre',
+      },
     },
     {
       path: '/country',
       name: 'country',
       component: () =>
         import(/* webpackChunkName: "country page" */ '@/views/Country.vue'),
+      meta: {
+        title: 'Country',
+      },
     },
     {
       path: '/movies',
       name: 'movies',
       component: () =>
         import(/* webpackChunkName: "movies page" */ '@/views/Movies.vue'),
+      meta: {
+        title: 'Movies',
+      },
     },
     {
       path: '/series',
       name: 'series',
       component: () =>
         import(/* webpackChunkName: "series page" */ '@/views/Series.vue'),
+      meta: {
+        title: 'TV-Series',
+      },
     },
     {
       path: '/top-imdb',
       name: 'top-imdb',
       component: () =>
         import(/* webpackChunkName: "top-imdb page" */ '@/views/TopIMDB.vue'),
+      meta: {
+        title: 'Top IMDB',
+      },
     },
     {
       path: '/:type/:id',
@@ -46,8 +64,16 @@ const router = createRouter({
         import(
           /* webpackChunkName: "card content" */ '@/components/CardContent.vue'
         ),
+      meta: {
+        title: 'Watching...🎬🍿🥤🎞️',
+      },
     },
   ],
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title}`
+  next()
 })
 
 export default router

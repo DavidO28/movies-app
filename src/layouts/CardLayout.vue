0ba3647bd44c53af552fd93e554ec88524c159b0
DavidO28 <damers22@yahoo.com>
2025-01-10 10:58:07 +0400

    added pagination

--- src/layouts/CardLayout.vue
+++ src/layouts/CardLayout.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-row justify="center" >
+  <v-row justify="center">
     <v-col
-      v-for="item in listData"
+      v-for="item in paginatedListData"
       :key="item.id"
       cols="12"
       sm="6"
@@ -11,15 +11,41 @@
     >
       <Card :movie="item" />
     </v-col>
+
+    <v-col
+      cols="12"
+      class="text-center"
+    >
+      <v-pagination
+        v-model="currentPage"
+        :length="pageCount"
+        :total-items="props.listData.length"
+        :items-per-page="itemsPerPage"
+      />
+    </v-col>
   </v-row>
 </template>
 
 <script setup lang="ts">
-  import { defineProps } from 'vue'
+  import { ref, computed } from 'vue'
   import Card from '@/components/Card.vue'
   import type { Movie } from '@/types'
 
-  defineProps<{
+  const props = defineProps<{
     listData: Movie[]
   }>()
+
+  const itemsPerPage = 12
+
+  const currentPage = ref(1)
+
+  const pageCount = computed(() => {
+    return Math.ceil(props.listData.length / itemsPerPage)
+  })
+
+  const paginatedListData = computed(() => {
+    const start = (currentPage.value - 1) * itemsPerPage
+    const end = start + itemsPerPage
+    return props.listData.slice(start, end)
+  })
 </script>
